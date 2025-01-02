4b1160cebd2cbfcdf9c2845b9272ee3df93ad4bd
Amir Kamel <amir.kghabrial@gmail.com>
2025-01-02 15:36:02 +0200

    add logic contact us

--- Contact Us/Contact Us.js
+++ Contact Us/Contact Us.js
@@ -1,13 +1,16 @@
 
-//using jQuery to validate the contact form
 $(document).ready(function () {
   $("#formcontact").on("submit", function (event) {
-    let isValid = false;
+    event.preventDefault(); // prevent the default not to refresh the page to show the message toast after submit
+
+    let isValid = true;
 
     const nameinput = $("#name").val().trim();
     const emailinput = $("#email").val().trim();
+    const subjectinput = $("#subject").val().trim();
+    const messageinput = $("#message").val().trim();
 
-//validate name
+    // Validate name
     if (!/^[a-zA-Z\s]{3,}$/.test(nameinput)) {
       isValid = false;
       $("#errorname").removeClass("d-none");
@@ -15,17 +18,28 @@ $(document).ready(function () {
       $("#errorname").addClass("d-none");
     }
 
-//validate email address
-    if (!/^[a-zA-Z0-9._%+-]{3,}@[a-zA-Z0-9.-]{3,}\.[a-zA-Z]{2,}$/.test(emailinput)) {
+    // Validate email address
+    if (
+      !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]{2,}\.[a-zA-Z]{2,}$/.test(emailinput)
+    ) {
       isValid = false;
       $("#erroremail").removeClass("d-none");
     } else {
       $("#erroremail").addClass("d-none");
     }
 
-//prevent form submission if any of the fields are invalid
-    if (!isValid) {
-      event.preventDefault();
+    // If all validations pass
+    if (isValid) {
+      // Save data to local storage
+      localStorage.setItem("Contact Name Data", JSON.stringify(nameinput));
+      localStorage.setItem("Contact Email Data", JSON.stringify(emailinput));
+      localStorage.setItem("Contact Subject Data", JSON.stringify(subjectinput));
+      localStorage.setItem("Contact Message Data", JSON.stringify(messageinput));
+      // show message toast
+      $(".toast").toast("show");
+
+      //clear the form
+      $("#formcontact")[0].reset();
     }
   });
 });
