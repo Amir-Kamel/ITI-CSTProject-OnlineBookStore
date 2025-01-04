f5f7e5260822378bd973f872377ed37ed6288ae4
Ahmed Mohamed Moqawi <ahmedmuqawi@gmail.com>
2025-01-04 19:51:16 +0200

    adding some logic to loadmore page

--- Assests/javascribt/Loadmore.js
+++ Assests/javascribt/Loadmore.js
@@ -1,3 +1,6 @@
+lastChoosenCategory = null;
+minPrice = 1e6;
+maxPrice = 0;
 $(function () {
   //create an array of objects that contains each category name and its quantity
   // get all products
@@ -5,22 +8,19 @@ $(function () {
   // console.log(allProducts);
   storeProductCategory(allProducts);
   let productsCategories = getProductCategory();
-  console.log(productsCategories);
+  // console.log(productsCategories);
   //display number of products in the categories-container div
   const container = $("#categories-container");
-  container.fadeOut(300, function () {
-    container.empty();
+  console.log("before");
+  fillBookCategory(container, allProducts, productsCategories);
 
-    for (let key in productsCategories) {
-      console.log(key, productsCategories[key]);
-    }
-  });
-
+  // display products based on category choosen
+  console.log("after");
   $("#slider-range").slider({
     range: true,
-    min: 0,
-    max: 500,
-    values: [0, 500],
+    min: minPrice,
+    max: maxPrice,
+    values: [minPrice, maxPrice],
     slide: function (event, ui) {
       // console.log(ui);
       $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
@@ -90,4 +90,124 @@ function getProductCategory() {
   const storedData = localStorage.getItem("productsCategories");
   // console.log("Retrieved data from localStorage:", storedData);
   return JSON.parse(storedData);
+}
+
+function fillBookCategory(container, allProducts, productsCategories) {
+  container.fadeOut(300, function () {
+    container.empty();
+
+    for (let key in productsCategories) {
+      // console.log(key, productsCategories[key]);
+      length = productsCategories[key].length;
+      // console.log(length);
+      let categoryLi = $("<li>");
+      categoryLi.text(key.charAt(0).toUpperCase() + key.slice(1));
+      let quantityLi = $("<li>");
+      quantityLi.text(productsCategories[key].length);
+      let ulContainer = $("<ul>");
+      ulContainer.addClass("list-unstyled counter d-flex justify-content-between fs-5");
+      if (key == "all") {
+        lastChoosenCategory = ulContainer;
+        ulContainer.addClass("text-primary fs-4");
+      }
+      ulContainer.append(categoryLi);
+      ulContainer.append(quantityLi);
+      ulContainer.on("click", function () {
+        // console.log($(this).text());
+        if (lastChoosenCategory) {
+          lastChoosenCategory.removeClass("text-primary fs-4");
+          lastChoosenCategory = $(this);
+          lastChoosenCategory.addClass("text-primary fs-4");
+          fillProductsContainer(allProducts, productsCategories, $(lastChoosenCategory.children()[0]).text().toLowerCase());
+        }
+      });
+      container.append(ulContainer);
+    }
+
+    fillProductsContainer(allProducts, productsCategories, $(lastChoosenCategory.children()[0]).text().toLowerCase());
+  });
+  container.fadeIn(300);
+}
+
+function fillProductsContainer(products, productsCategories, category) {
+  let productsContainer = $("#products-container");
+  // console.log(products);
+  // console.log(productsCategories);
+  // console.log(category);
+  productsContainer.fadeOut(300, function () {
+    productsContainer.empty();
+    productsCategories[category].forEach((productId) => {
+      // console.log(productId);
+      let product = products[productId];
+      if (minPrice > product.price) {
+        minPrice = product.price;
+        console.log("min" + minPrice);
+      }
+      if (maxPrice < product.price) {
+        maxPrice = product.price;
+        console.log("max" + maxPrice);
+      }
+      // image container
+      img = $("<img />");
+      img.prop("src", product.img_src);
+      img.prop("alt", product.title);
+      img.addClass("card-img-top");
+      //overlay container
+      let cartButton = $("<button>");
+      cartButton.addClass("btn cart btn-outline-secondary btn-lg");
+      let cartShape = $("<i>");
+      cartShape.addClass("fas fs-2 fa-cart-plus");
+      cartButton.append(cartShape);
+      let favButton = $("<button>");
+      favButton.addClass("btn wish-list btn-outline-secondary btn-lg");
+      let favShape = $("<i>");
+      favShape.addClass("far fs-2 fa-heart");
+      favButton.append(favShape);
+      let overlay = $("<div>");
+      overlay.addClass("overlay");
+      overlay.append(cartButton);
+      overlay.append(favButton);
+      let imgContainer = $("<div>");
+      imgContainer.addClass("img-container");
+      imgContainer.append(img);
+      imgContainer.append(overlay);
+      //card container
+      let productTitle = $("<h5>");
+      productTitle.addClass("card-title");
+      productTitle.text(product.title);
+      let productDescription = $("<p>");
+      productDescription.addClass("card-text");
+      productDescription.text(product.description);
+      let productPrice = $("<p>");
+      productPrice.addClass("card-text text-success fw-bold");
+      productPrice.text("price: $" + product.price);
+      let cardBody = $("<div>");
+      cardBody.addClass("card-body");
+      cardBody.append(productTitle);
+      cardBody.append(productDescription);
+      cardBody.append(productPrice);
+
+      // card
+      let card = $("<div>");
+      card.addClass("card");
+      card.append(imgContainer);
+      card.append(cardBody);
+      // append to products-container
+      productsContainer.append(card);
+      // container that contain the responsive classes
+      let container = $("<div>");
+      container.addClass("col-lg-4 col-md-6 col-sm-12 p-0v");
+      container.append(card);
+      productsContainer.append(container);
+    });
+    $(".cart").on("click", function () {
+      console.log(this);
+      // console.log("from cart");
+    });
+    $(".wish-list").on("click", function () {
+      console.log(this);
+      // console.log("from wish list");
+    });
+  });
+  productsContainer.fadeIn(300);
 }
